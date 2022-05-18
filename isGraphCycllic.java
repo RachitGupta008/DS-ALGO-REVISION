8972d63fc4fc9518d4e00a2272937564d1cb1b83
RachitGupta008 <35948433+RachitGupta008@users.noreply.github.com>
2022-05-18 17:48:31 +0530

    Create isGraphCycllic.java

---
+++ isGraphCycllic.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt) {
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+      Queue<Integer> q = new ArrayDeque<>();
+      boolean[] visited = new boolean[graph.length];
+      
+
+      for(int i=0;i<graph.length;i++){
+         if(visited[i]){
+            continue;
+         }
+         q.add(i);
+
+         while(!q.isEmpty()){
+
+         int elem = q.remove();
+         if(!visited[elem]){
+            visited[elem] = true;
+         }else{
+            System.out.println("true");
+            return;
+         }
+        
+         for(Edge e : graph[elem]){
+         //   System.out.println(e.nbr);
+            if(!visited[e.nbr]){
+               q.add(e.nbr);
+            }
+
+         }
+       //  System.out.println(q);
+
+      }
+      }
+      
+     System.out.println("false");
+
+      // write your code here
+   }
+}
