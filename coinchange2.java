2ae8923d12e0a4da5a07d73ab1771066aa725091
RachitGupta008 <35948433+RachitGupta008@users.noreply.github.com>
2022-05-30 12:16:31 +0530

    Create coinchange2.java

---
+++ coinchange2.java
@@ -0,0 +1,50 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void coinChange(int i, int[] coins, int amtsf, int tamt, String asf) {
+        // write your code here
+
+        if(amtsf == tamt){
+            System.out.println(asf+".");
+            return;
+        }
+
+        if(i==coins.length){
+            return;
+        }
+
+        int amtrem = tamt-amtsf;
+        int j=0;
+        String str = "";
+        while(amtrem>=coins[i]){
+            str+=coins[i]+"-";
+            j++;
+            amtrem = tamt - (amtsf + j*coins[i]);
+            coinChange(i+1, coins, amtsf+j*coins[i], tamt, asf+str);
+
+
+
+
+
+
+        }
+
+        coinChange(i+1, coins, amtsf, tamt, asf);
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+        int amt = Integer.parseInt(br.readLine());
+        coinChange(0, coins, 0, amt, "");
+    }
+}
