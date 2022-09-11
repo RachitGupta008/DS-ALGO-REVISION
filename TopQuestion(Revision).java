f1f6f76113651857a42721f866f34094487c83ff
RachitGupta008 <35948433+RachitGupta008@users.noreply.github.com>
2022-09-11 15:27:54 +0530

    Create TopQuestion(Revision).java

---
+++ TopQuestion(Revision).java
@@ -0,0 +1,7 @@
+Priority Queue : 
+https://leetcode.com/problems/maximum-performance-of-a-team/discuss/539680/Java-Detailed-Explanation-PriorityQueue-O(NlogN)
+https://leetcode.com/problems/divide-intervals-into-minimum-number-of-groups/discuss/2560875/Intuitive-and-concise-Priority-Queue-solution.
+
+
+DP:
+https://leetcode.com/submissions/detail/795999447/ -- Stock Buy Sell 2
