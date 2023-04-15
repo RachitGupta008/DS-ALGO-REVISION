52b58f297a919fdd0985aefaf0a3c1d654a33a4c
RachitGupta008 <35948433+RachitGupta008@users.noreply.github.com>
2023-04-15 15:46:52 +0530

    Update subsets2.java

--- pattern/subsets/subsets2.java
+++ pattern/subsets/subsets2.java
@@ -1,57 +1,64 @@
-// The logic reside in the backtracking tree, can use prev and new logic alsoe both will work.
+Time Complexity: The time complexity of this algorithm is O(2^n * n). Here's why:
+
+There are 2^n possible subsets for an array of n elements, as each element can either be included or excluded from a subset.
+
+In the worst case, the algorithm needs to iterate through all the subsets at each level of recursion. Since there are n levels of recursion (one for each element in the input array), the total number of iterations is 2^n * n.
+
+So, the overall time complexity of this algorithm is O(2^n * n).
+    
+// This class generates all possible subsets of an input array of integers,
+// ensuring that duplicates are not included in the final result.
 class Solution {
+    // Main function that takes an integer array and returns a list of lists,
+    // representing all unique subsets.
     public List<List<Integer>> subsets(int[] nums) {
         Set<String> set = new HashSet<>();
-        Arrays.sort(nums);
+        Arrays.sort(nums); // Sort the input array to help eliminate duplicates later
         
-        List<List<Integer>> ans= new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         ans.add(new ArrayList<>());
-        Set<List<Integer>> fans= new HashSet<>();
+        Set<List<Integer>> fans = new HashSet<>();
         f(nums, 0, ans, fans);
         
-        List<List<Integer>> sans= new ArrayList<>();
+        List<List<Integer>> sans = new ArrayList<>();
         
-        
-        for(List<Integer> li :  fans){
-                sans.add(li);
+        // Add all unique subsets from the HashSet to the final result list
+        for (List<Integer> li : fans) {
+            sans.add(li);
         }
         
         return sans;
     }
     
-   
-    
-    void f(int[] nums, int i, List<List<Integer>> ans, Set<List<Integer>> fans){
-        
-        if(i==nums.length){
-           fans.addAll(ans);
-           return;
+    // Recursive helper function to generate subsets
+    void f(int[] nums, int i, List<List<Integer>> ans, Set<List<Integer>> fans) {
+        if (i == nums.length) {
+            // If the index i reaches the end of the input array,
+            // add all subsets generated so far to the final set
+            fans.addAll(ans);
+            return;
         }
         List<List<Integer>> nans = new ArrayList<>();
         
-        for(List<Integer> li : ans){
-            
+        // Iterate through all current subsets
+        for (List<Integer> li : ans) {
             List<Integer> temp = new ArrayList<>();
-             List<Integer> temp1 = new ArrayList<>();
+            List<Integer> temp1 = new ArrayList<>();
             
             temp.addAll(li);
             temp1.addAll(li);
             
-            if(temp.size()==0 || (temp.size()>0 && temp.get(temp.size()-1)!=nums[i])){
-                //temp.add(nums[i]);
+            // Only add the new subset if it doesn't duplicate the previous element
+            if (temp.size() == 0 || (temp.size() > 0 && temp.get(temp.size() - 1) != nums[i])) {
                 nans.add(temp);
-                
             }
+            
+            // Add the current element to the subset and store it in nans
             temp1.add(nums[i]);
             nans.add(temp1);
-            
         }
         
-        
-        f(nums, i+1, nans, fans);
-        
-       
-        
+        // Recursively call the function with the updated subsets and increment the index
+        f(nums, i + 1, nans, fans);
     }
-    
 }
